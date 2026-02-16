78e93df5ec949d04b278808a9dd8354f178df370
Sajni Bercel <sajnibercel@gmail.com>
2026-02-16 18:24:42 +0100

    install.sh added
    install.sh uses pacman for install the basic and some extra packages

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo pacman -Syu
+sudo pacman --needed -S xorg i3 rofi alacritty \
+    stow mpv openssh git ttf-iosevka-nerd nvim \
+    zip unzip make gcc firefox fastfetch htop  \
+    btop man mc ripgrep tar picom
