57e7d908e2372302b4b2b1047d85c59c2b6f1d60
Sajni Bercel <sajnibercel@gmail.com>
2026-02-22 22:41:01 +0100

    chooseable sys and extra update, chooseable installs

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-sudo pacman -Syu
-sudo pacman --needed -S xorg i3 rofi alacritty \
+echo "This instalation is arch linux (pacman) only"
+read -p "Do u want to update the system (Y/n): " confirm
+
+if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
+    sudo pacman -Syu
+fi
+
+read -p "Do u want to install same basic programs? (Y/n): " confirm
+if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
+sudo pacman --needed -S xorg i3 i3status rofi alacritty \
     stow mpv openssh git ttf-iosevka-nerd nvim \
     zip unzip make gcc firefox fastfetch htop  \
     btop man mc ripgrep xorg-xinit ffmpeg
+fi
+
+read -p "Do u want to make the symlinks with stow? (Y/n): " confirm
+if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
+    echo "TODO"
+fi
