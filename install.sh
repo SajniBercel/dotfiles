8da68323b50ef6ca683b1f85a74546ce02c57345
Sajni Bercel <sajnibercel@gmail.com>
2026-02-24 11:26:19 +0100

    remove command

--- install.sh
+++ install.sh
@@ -11,10 +11,26 @@ if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
 sudo pacman --needed -S xorg i3 i3status rofi alacritty \
     stow mpv openssh git ttf-iosevka-nerd nvim \
     zip unzip make gcc firefox fastfetch htop  \
-    btop man mc ripgrep xorg-xinit ffmpeg
+    btop man mc ripgrep xorg-xinit ffmpeg npm
 fi
 
 read -p "Do u want to make the symlinks with stow? (Y/n): " confirm
 if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
-    echo "TODO"
+    read -p "Do u want to make the symlinks with stow? (Y/n): " confirm
+    if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]] then
+        rm -rf ~/.bashrc \
+        ~/.config/i3/* \
+        ~/.config/i3status/*
+        ~/.config/alacritty/*
+        ~/.config/nvim/*
+        ~/.config/rofi/*
+        ~/.xinitrc
+    fi
+    stow alacritty
+    stow bash
+    stow i3
+    stow i3status
+    stow nvim
+    stow rofi
+    stow xinit
 fi
