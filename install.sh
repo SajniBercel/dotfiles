91c78184aca3cf784a4aea49ea7e76930bc931d2
Sajni Bercel <sajnibercel@gmail.com>
2026-02-24 11:45:33 +0100

    small corrections

--- install.sh
+++ install.sh
@@ -6,25 +6,25 @@ if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
     sudo pacman -Syu
 fi
 
-read -p "Do u want to install same basic programs? (Y/n): " confirm
+read -p "Do u want to install basic programs (required most of them required for the configs)? (Y/n): " confirm
 if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
-sudo pacman --needed -S xorg i3 i3status rofi alacritty \
-    stow mpv openssh git ttf-iosevka-nerd nvim \
-    zip unzip make gcc firefox fastfetch htop  \
-    btop man mc ripgrep xorg-xinit ffmpeg npm
+    sudo pacman --needed -S xorg i3 i3status rofi alacritty \
+        stow mpv openssh git ttf-iosevka-nerd nvim \
+        zip unzip make gcc firefox fastfetch htop  \
+        btop man mc ripgrep xorg-xinit ffmpeg npm
 fi
 
 read -p "Do u want to make the symlinks with stow? (Y/n): " confirm
 if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
-    read -p "Do u want to make the symlinks with stow? (Y/n): " confirm
-    if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]] then
-        rm -rf ~/.bashrc \
-        ~/.config/i3/* \
-        ~/.config/i3status/*
-        ~/.config/alacritty/*
-        ~/.config/nvim/*
-        ~/.config/rofi/*
-        ~/.xinitrc
+    read -p "Do u want to remove the current configs (gnu stow will fail if the file already exists)? (Y/n): " confirm
+    if [[ "$confirm" =~ ^([Yy][Ee][Ss]|[Yy])$ || -z "$confirm" ]]; then
+        sudo rm -rf ~/.bashrc \
+            ~/.config/i3/* \
+            ~/.config/i3status/*
+            ~/.config/alacritty/*
+            ~/.config/nvim/*
+            ~/.config/rofi/*
+            ~/.xinitrc
     fi
     stow alacritty
     stow bash
